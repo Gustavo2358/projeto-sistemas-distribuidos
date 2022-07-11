e80195af72cc9496b6f9ac49cba701b52f0e2116
Gustavo <gustavo28064212@gmail.com>
2022-07-10 22:37:09 -0300

    servidor recebe infos do peer

--- src/Peer.java
+++ src/Peer.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -36,26 +36,15 @@ public class Peer {
                     leave = true;
                     break;
                 default:
-                    System.out.println("Digíte uma opção válida (entre 1 e 4)");
+                    System.out.println("Digite uma opção válida (entre 1 e 4)");
                     break;
             }
         } while (!leave);
 
     }
 
-    private static void join() {
-        getInfo();
-        //String com as infos
-        StringBuilder stringArquivos = new StringBuilder();
-        for(String f : filesList)
-            stringArquivos.append(f).append(" ");
-        String infos = String.format("Sou peer %s:%s com arquivos %s",
-                ip.toString().substring(1), port, stringArquivos);
-        System.out.println(infos);
-    }
-
     private static int menu() {
-        System.out.println("Digíte a opção desejada:");
+        System.out.println("Digite a opção desejada:");
         System.out.println("1 - JOIN");
         System.out.println("2 - SEARCH");
         System.out.println("3 - DOWNLOAD");
@@ -65,38 +54,117 @@ public class Peer {
         int opt;
         do {
             optString = sc.nextLine();
-            opt = validate(optString);
+            opt = validateMenuOption(optString);
         } while( opt == -1);
         return opt;
     }
 
-    private static int validate(String optString) {
+    private static int validateMenuOption(String optString) {
         int opt;
         try {
             opt = Integer.parseInt(optString);
         }catch (NumberFormatException e){
-            System.out.println("Digíte um valor numérico");
+            System.out.println("Digite um valor numérico");
             return -1;
         }
         return opt;
     }
 
+    private static void join() {
+        getInfo();
+        sendJoinRequest();
+        receiveJoinResponse();
+        printInfo();
+    }
+
+    private static void receiveJoinResponse() {
+        try {
+            DatagramSocket clientSocket = new DatagramSocket(port);
+            byte[] recBuffer = new byte[1024];
+            DatagramPacket recPkt = new DatagramPacket(recBuffer, recBuffer.length);
+            clientSocket.receive(recPkt);
+            System.out.println(new String(recPkt.getData(), recPkt.getOffset(), recPkt.getLength()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendJoinRequest() {
+        try {
+            DatagramSocket clientSocket = new DatagramSocket(port);
+            InetAddress serverIpAddress = InetAddress.getByName("127.0.0.1");
+            //TODO enviar objeto Mensagem ao invés da string
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("JOIN").append(" ").append(ip.toString()).append(" ").append(port);
+            for(String file: filesList)
+                sb.append(" ").append(file);
+            byte[] sendData = sb.toString().getBytes(StandardCharsets.UTF_8);
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverIpAddress, 10098 );
+            clientSocket.send(sendPacket);
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    private static void printInfo() {
+        //String com as infos
+        StringBuilder stringArquivos = new StringBuilder();
+        for(String f : filesList)
+            stringArquivos.append(f).append(" ");
+        String infos = String.format("Sou peer %s:%s com arquivos %s",
+                ip.toString(), port, stringArquivos);
+        System.out.println(infos);
+    }
+
     private static void getInfo() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Endereço de Ip:");
+        getAddress(sc);
+        System.out.println("Porta:");
+        getPort(sc);
+        System.out.println("Nome da pasta: ");
+        selectDirectory(sc);
+        filesList = getFilesInDirectory(directoryPath);
+    }
+
+    private static void getAddress(Scanner sc) {
         String ipString = sc.nextLine();
         try {
             ip = InetAddress.getByName(ipString);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
-        System.out.println("Porta:");
-        port = Integer.parseInt(sc.nextLine());
-        selectDirectory(sc);
-        filesList = listFilesInFolder(directoryPath);
     }
 
-    private static List<String> listFilesInFolder(Path directoryPath) {
+    private static void getPort(Scanner sc) {
+        port = Integer.parseInt(sc.nextLine());
+    }
+
+    private static void selectDirectory(Scanner sc){
+        do {
+            String directoryName = sc.nextLine();
+            directoryPath = Paths.get(directoryName);
+            try {
+                if (!Files.isDirectory(Paths.get(directoryName))) {
+                    System.out.println("O diretório ainda não existe, criando novo diretório...");
+                    Files.createDirectory(Paths.get(directoryName));
+                    System.out.println("Diretório criado com sucesso.");
+                    return;
+                }
+                System.out.println("Diretório selecionado.");
+                return;
+            } catch (java.io.IOException e) {
+                System.out.println("Erro para criar...digite um caminho válido");
+            }
+        }while(true);
+    }
+
+    private static List<String> getFilesInDirectory(Path directoryPath) {
         try (Stream<Path> files = Files.walk(Paths.get(directoryPath.toString()))) {
             return  files
                     .filter(Files::isRegularFile)
@@ -109,22 +177,4 @@ public class Peer {
         }
     }
 
-    private static void selectDirectory(Scanner sc){
-        do {
-            System.out.println("Nome da pasta: ");
-            String directoryName = sc.nextLine();
-            try {
-                if (!Files.isDirectory(Paths.get(directoryName))) {
-                    System.out.println("O diretório ainda não existe, criando novo diretório...");
-                    Files.createDirectory(Paths.get(directoryName));
-                    System.out.println("Diretório criado com sucesso.");
-                    directoryPath = Paths.get(directoryName);
-                    return;
-                }
-                System.out.println("Diretório selecionado.");
-            } catch (java.io.IOException e) {
-                System.out.println("Erro para criar...digite um caminho válido");
-            }
-        }while(true);
-    }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,5 +1,28 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.nio.charset.StandardCharsets;
+
 public class Servidor {
     public static void main(String[] args) {
+        try {
+            DatagramSocket serverSocket = new DatagramSocket(10098);
+            while(true){
 
+                byte[] recBuffer = new byte[1024];
+                DatagramPacket recPkt = new DatagramPacket(recBuffer, recBuffer.length);
+                serverSocket.receive(recPkt);
+                String recData = new String(recPkt.getData(),recPkt.getOffset(),recPkt.getLength());
+                //TODO Quando receber o JOIN, print “Peer [IP]:[porta] adicionado com arquivos [só nomes dos arquivos].
+                System.out.println(recPkt.getAddress() + " " + recPkt.getPort());
+                System.out.println(recData);
+
+                byte[] sendBuf = "JOIN_OK".getBytes(StandardCharsets.UTF_8);
+                DatagramPacket sendPacket = new DatagramPacket(sendBuf,sendBuf.length,recPkt.getAddress(), recPkt.getPort());
+                serverSocket.send(sendPacket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
